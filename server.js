869f038c64bd1f1216e65c54647d318913774568
dangquocdung <dungthinhvn@gmail.com>
2016-12-07 17:32:44 +0700

    xac thuc local

---
+++ server.js
@@ -0,0 +1,48 @@
+// server.js
+
+// set up ======================================================================
+// Lấy các công cụ cần thiết
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // Kết nối đến db thông qua biến url đã tạo tại ./config/database.js
+
+require('./config/passport')(passport); // lấy cấu hình passport tại ./config/passport
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+
+// required for passport
+app.use(session({
+    secret: 'dangquocdung1234567890', // session secret
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
